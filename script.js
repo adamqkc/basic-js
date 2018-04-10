349579bfeaa7ad3f8a3d553d702aaacbf35c1744
Adam Chen <adamsfpro@Adams-MacBook-Pro.local>
2018-04-10 09:36:24 -0700

    Finish basic-js function problems

--- script.js
+++ script.js
@@ -1,30 +1,91 @@
 module.exports = {
-  difference: function(){
-
+  difference: function(x, y){
+    return x - y;
   },
-  isEqual: function(){
-
+  isEqual: function(x, y){
+    return (x === y);
   },
-  isEven: function(){
-
+  isEven: function(x){
+    if (x % 2 === 0) {
+      return true;
+    } else {
+      return false;
+    }
   },
-  isDivisible: function(){
-
+  isDivisible: function(x, y){
+    if (x % y === 0) {
+      return true;
+    } else {
+      return false;
+    }
   },
-  discountPercentage: function(){
-
+  discountPercentage: function(amount, discount){
+    if (discount >= 0 && discount <= 100) {
+      return (amount * (discount/100));
+    } else {
+      return "please enter a number between 0 and 100";
+    }
   },
-  isVowel: function(){
-
+  isVowel: function(letter){
+    if (letter === 'a' ||
+        letter === 'e' ||
+        letter === 'i' ||
+        letter === 'o' ||
+        letter === 'u') {
+          return true;
+    } else {
+      return false;
+    }
   },
-  celsiusToFahrenheit: function(){
-
+  celsiusToFahrenheit: function(celsius){
+    return celsius * 1.8 + 32;
   },
-  biggestOfThree: function(){
-
+  biggestOfThree: function(x, y, z){
+    var array = [x, y, z];
+    var biggestNum = null;
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] > biggestNum) {
+        biggestNum = array[i];
+      }
+    }
+    return biggestNum;
   },
   // Bonus
-  federalIncomeTaxCalculator: function(){
+  federalIncomeTaxCalculator: function(income) {
+    var taxSum = 0;
 
+    while (income > 0) {
+      var difference = 0;
+
+      if (income > 418400) {
+        difference = income - 418400;
+        taxSum = taxSum + (difference * 0.396);
+        income -= difference;
+      } else if (income > 416700) {
+        difference = income - 416700;
+        taxSum = taxSum + (difference * 0.35);
+        income -= difference;
+      } else if (income > 191650) {
+        difference = income - 191650;
+        taxSum = taxSum + (difference * 0.33);
+        income -= difference;
+      } else if (income > 91900) {
+        difference = income - 91900;
+        taxSum = taxSum + (difference * 0.28);
+        income -= difference;
+      } else if (income > 37950) {
+        difference = income - 37950;
+        taxSum = taxSum + (difference * 0.25);
+        income -= difference;
+      } else if (income > 9325) {
+        difference = income - 9325;
+        taxSum = taxSum + (difference * 0.15);
+        income -= difference;
+      } else {
+        taxSum = taxSum + (income * 0.10);
+        income -= 9325;
+      }
+    }
+    return taxSum;
   }
-};
+}
